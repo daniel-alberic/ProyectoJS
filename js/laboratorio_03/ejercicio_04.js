7d2f7eb071a1dab4b2de8798487b0ea194c73ac8
Daniel Espinoza <dani619349@gmail.com>
2026-09-23 08:57:50 -0500

    Cree una sub carpeta (laboratorio03) y avanze con 5 ejercicios ahi

---
+++ js/laboratorio_03/ejercicio_04.js
@@ -0,0 +1,9 @@
+let edadPersona = prompt("Ingrese su edad");
+let paisOrigen = prompt("Ingrese su pais");
+
+if (edadPersona >= 65 && paisOrigen ){
+    console.log("Accede al bono!");
+}
+else {
+    console.log("No accede al bono!");
+}
---
+++ js/laboratorio_03/ejercicio_05.js
@@ -0,0 +1,11 @@
+
+//Eres estudiante o instructor?
+let user = prompt("Eres estudiante o instructor?");
+
+if (user == "estudiante") {
+    console.log("Ganaste un descuento del 15%");
+}
+
+else {
+    console.log("No tienes dsct");
+}
