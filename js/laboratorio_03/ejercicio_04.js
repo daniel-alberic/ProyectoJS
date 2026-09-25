784e735283381dec0f14f3de2f0cc2730082be65
Daniel Espinoza <dani619349@gmail.com>
2026-09-25 18:01:28 -0500

    Volvi a  arreglar el desordern

--- js/laboratorio_03/ejercicio_04.js
+++
@@ -1,9 +0,0 @@
-let edadPersona = prompt("Ingrese su edad");
-let paisOrigen = prompt("Ingrese su pais");
-
-if (edadPersona >= 65 && paisOrigen ){
-    console.log("Accede al bono!");
-}
-else {
-    console.log("No accede al bono!");
-}
